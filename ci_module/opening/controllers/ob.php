0209e523b5de3078bc6c8bacca79e2278d2aff38
yamaaja <yamapocong@gmail.com>
2018-03-28 14:14:53 +0700

    up

--- ci_module/opening/controllers/ob.php
+++ ci_module/opening/controllers/ob.php
@@ -455,7 +455,7 @@ class OpeningOb
         // display_db_pager($table);
 
         box_footer_start();
-        echo '<a title="Add new item" class="ajaxsubmit btn green btn_right" href="' . site_url('opening/'.$this->type.'/add') . '"><i class="material-icons">add</i> Add </a>';
+        echo '<a title="Add new item" class="ajaxsubmit btn green btn_right waves-effect waves-light" href="' . site_url('opening/'.$this->type.'/add') . '">Add </a>';
         box_footer_end();
     }
 
