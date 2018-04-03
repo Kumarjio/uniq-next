57aca4b4ee1fdb5889ff096bd4fc7517a56a05f9
Megan <megareceiver@gmail.com>
2018-04-03 16:37:55 +0700

    up

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -15,9 +15,7 @@
 //     });
 // };
 
-// $(document).ready(function(){
-//     // $('.modal').modal().show();
-//     // var instance = M.Modal.getInstance('.modal');
-//     // instance.open();
-//     // console.log('nah');
-//   });
+$(document).ready(function(){
+  $('.modal').modal();
+  $('#setup-wizard').modal('open');
+});
